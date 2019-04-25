3cb6cc00184479c72e813c540f668831c53b8f78
Paulus Esterhazy <pesterhazy@gmail.com>
2019-04-24 19:38:59 -0500

    WIP

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -3,46 +3,11 @@
             [clojure.string :as str]
             [reagent.core :as reagent :refer [atom]]))
 
-;; (println "This text is printed from src/hello_world/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; (defn multiply [a b] (* a b))
-
-
-;; ;; define your app data so that it doesn't get over-written on reload
-;; (defonce app-state (atom {:text "Hello world!"}))
-
-;; (defn get-app-element []
-;;   (gdom/getElement "app"))
-
-;; (defn hello-world []
-;;   [:div
-;;    [:h1 (:text @app-state)]
-;;    [:h3 "Foo"]])
-
-;; (defn mount [el]
-;;   (reagent/render-component [hello-world] el))
-
-;; (defn mount-app-element []
-;;   (when-let [el (get-app-element)]
-;;     (mount el)))
-
-;; ;; conditionally start your application based on the presence of an "app" element
-;; ;; this is particularly helpful for testing this ns without launching the app
-;; (mount-app-element)
-
-;; ;; specify reload hook with ^;after-load metadata
-;; (defn ^:after-load on-reload []
-;;   (mount-app-element)
-;;   ;; optionally touch your app-state to force rerendering depending on
-;;   ;; your application
-;;   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-;; )
-
-
-;; A detailed walk-through of this source code is provided in the docs:
-;; https://github.com/Day8/re-frame/blob/master/docs/CodeWalkthrough.md
+(defn ^:before-load on-reload []
+  (println "before-load"))
 
 ;; -- Domino 1 - Event Dispatch -----------------------------------------------
+
 
 (defn dispatch-timer-event
   []
